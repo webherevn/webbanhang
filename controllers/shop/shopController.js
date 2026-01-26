3acc9ee05a9417018e0601c27c8b13eafba8f3e3
Admin <webherevn@gmail.com>
2026-01-26 13:14:13 +0700

    Fix wrong import in ProductModel

--- controllers/shop/shopController.js
+++ controllers/shop/shopController.js
@@ -1,5 +1,6 @@
 // controllers/shop/shopController.js
 const Product = require('../../models/ProductModel'); // Nhớ gọi đúng tên Model
+const Category = require('../models/CategoryModel');
 
 exports.getHomepage = async (req, res) => {
   try {
@@ -38,4 +39,35 @@ exports.getProductDetail = async (req, res) => {
     console.log(err);
     res.status(500).send('Lỗi tải trang chi tiết');
   }
+};
+
+exports.getCategoryProducts = async (req, res) => {
+    try {
+        const slug = req.params.slug;
+
+        // 1. Tìm Danh mục theo Slug
+        const category = await Category.findOne({ slug: slug });
+
+        if (!category) {
+            // Nếu khách gõ bậy bạ -> Về trang 404 hoặc trang chủ
+            return res.status(404).render('404', { pageTitle: 'Không tìm thấy danh mục', path: '/404' });
+        }
+
+        // 2. Tìm Sản phẩm thuộc danh mục này
+        // Lưu ý: Trong ProductModel lúc lưu, bạn lưu field 'category' là slug hay tên?
+        // Code bên dưới giả định bạn lưu category là Slug (như logic bài trước chúng ta làm)
+        const products = await Product.find({ category: slug }).sort({ createdAt: -1 });
+
+        // 3. Render ra view
+        res.render('shop/category-products', { // Chúng ta sẽ tạo file view này ở Bước 4
+            pageTitle: category.name,
+            path: '/category',
+            category: category,
+            products: products
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    }
 };
