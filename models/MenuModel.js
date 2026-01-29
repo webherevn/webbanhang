b307da86ae970bdf70c9d5b3ea3bf2fcf068019f
Admin <webherevn@gmail.com>
2026-01-29 16:59:34 +0700

    Add missing blogController

--- models/MenuModel.js
+++ models/MenuModel.js
@@ -4,6 +4,10 @@ const menuSchema = new mongoose.Schema({
     name: { type: String, required: true }, // Tên hiển thị (VD: Trang chủ)
     link: { type: String, required: true }, // Đường dẫn (VD: /products)
     order: { type: Number, default: 0 },    // Thứ tự sắp xếp
+    // --- BẠN ĐANG THIẾU DÒNG NÀY ---
+    // Trường này dùng để xác định menu này là con của ai
+    parent: { type: mongoose.Schema.Types.ObjectId, ref: 'Menu', default: null },
+    // -------------------------------
     isActive: { type: Boolean, default: true } // Ẩn/Hiện
 });
 
